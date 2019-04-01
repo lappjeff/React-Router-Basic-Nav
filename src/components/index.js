ed1e79ae9f8c0298c2eb949462e1f7263979af5f
unknown <lappjeff20@gmail.com>
2019-04-01 15:07:43 -0400

    Initial commit. imported BrowserRouter

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
-
+import {BrowserRouter as Router};
 export { Home, About, Contact, Navigation };
